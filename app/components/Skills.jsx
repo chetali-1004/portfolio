c84848c1c9ccb513539f146106dc05be7634aac0
Chetali Goyal <goyalchetali123@gmail.com>
2024-09-02 20:17:14 +0530

    fixing build errors

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -18,7 +18,7 @@ const Skills = () => {
     <section className='mt-[160px] bg-[#313131] rounded-xl md:py-10 py-7 md:w-5/6 w-full mx-auto' id="skills">
       <div className='text-center'>
         <span className='text-3xl lg:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Skill Set</span>
-        <p className='text-[#ADB7BE] text-lg mt-5 mx-10 mb-7'>"Powered by expertise in leading-edge technologies and programming languages, I possess a versatile skill set to engineer innovative solutions, sparking triumph in dynamic endeavors."</p>
+        <p className='text-[#ADB7BE] text-lg mt-5 mx-10 mb-7'>&quot;Powered by expertise in leading-edge technologies and programming languages, I possess a versatile skill set to engineer innovative solutions, sparking triumph in dynamic endeavors.&quot;</p>
         <div className='skills-container mt-10'>
           <div className='skills-inner'>
             {combinedSkills.map((skill, index) => (
